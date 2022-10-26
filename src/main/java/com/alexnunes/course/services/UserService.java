3554fa68d92bec37ef50cc97d98655b58fb6dca2
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-26 05:45:53 -0300

    Service layer

---
+++ src/main/java/com/alexnunes/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.alexnunes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexnunes.course.entities.User;
+import com.alexnunes.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
