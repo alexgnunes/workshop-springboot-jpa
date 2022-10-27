b2c9ba255b3a1b5339d33d8d871f9a21da2967a0
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-27 05:19:12 -0300

    Implement order

---
+++ src/main/java/com/alexnunes/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.alexnunes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexnunes.course.entities.Order;
+import com.alexnunes.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
