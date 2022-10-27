0d02e3746b644c8a5cf15c222002ca6b739eaa50
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-27 06:18:28 -0300

    Category

---
+++ src/main/java/com/alexnunes/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.alexnunes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexnunes.course.entities.Category;
+import com.alexnunes.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
