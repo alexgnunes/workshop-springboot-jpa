3a7c8a37fa2d8ed235cb2c18b0ffbac99b4b07e8
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-28 05:18:23 -0300

    Product

---
+++ src/main/java/com/alexnunes/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.alexnunes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexnunes.course.entities.Product;
+import com.alexnunes.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
