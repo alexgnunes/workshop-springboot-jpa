83fd6636f463ee24fa04547b52712ed85980bf60
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-11-02 06:15:30 -0300

    Payment, one-to-one association

--- src/main/java/com/alexnunes/course/config/TestConfig.java
+++ src/main/java/com/alexnunes/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.alexnunes.course.entities.Category;
 import com.alexnunes.course.entities.Order;
 import com.alexnunes.course.entities.OrderItem;
+import com.alexnunes.course.entities.Payment;
 import com.alexnunes.course.entities.Product;
 import com.alexnunes.course.entities.User;
 import com.alexnunes.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
